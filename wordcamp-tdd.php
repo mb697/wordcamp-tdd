19e2ee2725c024576c9522266dfe7c481ffc20c0
Matt Bush <matt.mb697@gmail.com>
2018-10-17 10:30:54 +0100

    Plugin bootstrap file and test scaffolding.

---
+++ wordcamp-tdd.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin bootstrap file.
+ *
+ * @wordpress-plugin
+ * Plugin Name: WordCamp Test Driven Development.
+ * Version: 0.0.1
+ * Author: Matt Bush
+ *
+ * @package wordcamp-tdd
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
